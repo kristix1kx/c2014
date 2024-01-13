fc27c9f88e826d54e1188cfac95f00b40a3bdebe
c2014 <nikolv163@gmail.com>
2024-01-13 15:26:08 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,81 @@
-class StudingError(Exception):
-    def __str__(self):
-        return f"Стільки часу за день ви можете вчитися "
-def check_study(amount_of_study, limit_value):
-    if amount_of_studyl>limit_value:
-        return "Ви забагато навчаєтесь!"
-    else:
-        raise StudingError(amount_of_studytime)
-studytime=180
-check_studytime(studytime,240)
+import random
+class Human:
+    def __init__(self,name="Human",car=None, job=None, home=None):
+        self.name=name
+        self.money=100
+        self.gladness=50
+        self.satiety=50
+        self.job=job
+        self.home=home
+        self.car=car
+    def get_home(self):
+        self.home=House()
+    def get_job(self):
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job=Job(job_list)
+    def get_car(self):
+        self.car=Auto(brand_of_car)
+    def eat(self):
+        if self.home.food<=0:
+            self.shopping("food ")
+        else:
+            if self.satiety>=100:
+                self.satiety=100
+                return
+        self.satiety+=5
+        self.home.food-=5
+    def work(self):
+        pass
+    def shopping(self,manage):
+        pass
+    def chill(self):
+        pass
+    def clean_home(self):
+        pass
+    def to_repair(self):
+        pass
+    def days_indexes(self,day):
+        pass
+    def is_alive(self):
+        pass
+    def live(self):
+        pass
+class Auto:
+    def __init__(self, brand_list):
+        self.brand=random.choice(list(brand_list))
+        self.fuel=brand_list[self.brand]["fuel"]
+        self.strength = brand_list[self.brand]["strength"]
+        self.consumption = brand_list[self.brand]["consumption"]
+
+    def drive(self):
+        if self.strength>0 and self.fuel>=self.consumption:
+            self.fuel-=self.consumption
+            self.strength-=1
+            return True
+        else:
+            print("The car can not move")
+            return False
+class House:
+    def __init__(self):
+        self.food=0
+        self.mess=0
+brand_of_car={
+    "BMW": {"fuel": 100, "strength": 100, "consumption": 12},
+    "Toyota": {"fuel": 50, "strength": 60, "consumption": 10},
+    "Volvo": {"fuel": 70, "strength": 120, "consumption": 8},
+    "Ferrari": {"fuel": 80,"strength": 150, "consumption": 14}}
+job_list={
+    "Java developer": {"salery": 50, "gladness_less": 10},
+    "Python developer": {"salery": 40, "gladness_less": 3},
+    "C++ developer": {"salery": 45, "gladness_less": 25},
+    "Rust developer": {"salery": 70, "gladness_less": 1}}
+
+class Job:
+    def __init__(self,job_list):
+        self.job=random.choice(list(job_list))
+        self.salary=job_list[self.job]["salery"]
+        self.gladness_less = job_list[self.job]["gladness_less"]
